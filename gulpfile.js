1a380946c5607a72ea22dac9f8130c8d401cf5ba
Joshua Thomas <joshua.thomas@gmail.com>
2016-05-05 08:55:42 -0700

    first cut at adding eslint and units

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,45 @@
-'use strict';
-
+/*eslint no-invalid-this: 0*/
 var
+	del = require('del'),
+	eslint = require('gulp-eslint'),
 	gulp = require('gulp'),
-	jshint = require('gulp-jshint');
+	gulpUtil = require('gulp-util'),
+	istanbul = require('gulp-istanbul'),
+	mocha = require('gulp-mocha');
 
 
-gulp.task('jshint', function () {
-	return gulp
-		.src(['*.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish'));
-});
+module.exports = (() => {
+	'use strict';
+
+	gulp.task('clean', () => del('coverage', 'reports'));
+
+	gulp.task('lint', () => {
+		return gulp
+			.src(['**/*.js', '!node_modules/**', '!reports/**'])
+			.pipe(eslint())
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError());
+	});
+
+	gulp.task('test', ['clean', 'lint', 'test-unit']);
+
+	gulp.task('test-unit', ['clean'], () => {
+		return gulp
+			.src(['./promise.js'])
+			.pipe(istanbul())
+			.pipe(istanbul.hookRequire())
+			.on('finish', () => {
+				return gulp
+					.src(['./test/promise.js'])
+					.pipe(mocha({ reporter : 'spec' })
+						.on('error', function (err) {
+							if (err.showStack) {
+								gulpUtil.log(err);
+							}
+
+							return this.emit('end');
+						}))
+					.pipe(istanbul.writeReports('./reports'));
+			});
+	});
+})();
